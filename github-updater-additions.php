61fb625a84fd7b484cf06a6d2bd7fca8d8beee2c
Andy Fragen <andy@thefragens.com>
2020-01-27 14:27:23 -0800

    first pass settings

--- github-updater-additions.php
+++ github-updater-additions.php
@@ -24,118 +24,19 @@
  * Requires PHP:      5.6
  */
 
-namespace Fragen\GitHub_Updater;
+namespace Fragen\GitHub_Updater\Additions;
 
 use Fragen\Singleton;
 
-add_filter(
-	'github_updater_additions',
-	function( $false, $repos, $type ) {
-		$config    = file_get_contents( __DIR__ . '/github-updater-additions.json' );
-		$additions = new Additions();
-		$additions->register( $config, $repos, $type );
-
-		return $additions->add_to_github_updater;
-	},
-	10,
-	3
-);
-
-/**
- * Class Additions
- *
- * Add repos without required headers to GitHub Updater.
- * Uses JSON config data file and companion plugin.
- *
- * @uses \Fragen\Singleton
+/*
+ * Exit if called directly.
+ * PHP version check and exit.
  */
-class Additions {
-	/**
-	 * Holds array of plugin/theme headers to add to GitHub Updater.
-	 *
-	 * @access public
-	 * @var    array
-	 */
-	public $add_to_github_updater;
-
-	/**
-	 * Register JSON config file.
-	 *
-	 * @access public
-	 *
-	 * @param string $json_config The repo config, in JSON.
-	 * @param array  $repos       The repos to pull from.
-	 * @param string $type        The plugin type ('plugin' or 'theme').
-	 *
-	 * @return bool
-	 */
-	public function register( $json_config, $repos, $type ) {
-		if ( empty( $json_config ) ) {
-			return false;
-		}
-		if ( null === ( $config = json_decode( $json_config, true ) ) ) {
-			$error = new \WP_Error( 'json_invalid', 'JSON ' . json_last_error_msg() );
-			Singleton::get_instance( 'Messages', $this )->create_error_message( $error );
-
-			return false;
-		}
-
-		$this->add_headers( $config, $repos, $type );
-
-		return true;
-	}
-
-	/**
-	 * Add GitHub Updater headers to plugins/themes via a filter hooks.
-	 *
-	 * @access public
-	 * @uses   \Fragen\GitHub_Updater\Additions::add_to_github_updater()
-	 *
-	 * @param array  $config The repo config.
-	 * @param array  $repos  The repos to pull from.
-	 * @param string $type   The plugin type ('plugin' or 'theme').
-	 *
-	 * @return void
-	 */
-	public function add_headers( $config, $repos, $type ) {
-		foreach ( $config as $repo ) {
-			$addition  = [];
-			$additions = [];
-
-			$type      = explode( '_', $repo['type'] )[1];
-			$file_path = 'plugin' === $type ? WP_PLUGIN_DIR . "/{$repo['slug']}" : null;
-			$file_path = 'theme' === $type ? get_theme_root() . "/{$repo['slug']}/style.css" : $file_path;
-
-			if ( ! file_exists( $file_path ) ) {
-				continue;
-			}
-
-			$all_headers = Singleton::get_instance( 'Base', $this )->get_headers( $type );
-
-			$additions[ $repo['slug'] ]['type'] = $type;
-			$additions[ $repo['slug'] ]         = get_file_data( $file_path, $all_headers );
-
-			switch ( $repo['type'] ) {
-				case 'github_plugin':
-				case 'github_theme':
-					$addition[ 'GitHub' . ucwords( $type ) . 'URI' ] = $repo['uri'];
-					break;
-				case 'bitbucket_plugin':
-				case 'bitbucket_theme':
-					$addition[ 'Bitbucket' . ucwords( $type ) . 'URI' ] = $repo['uri'];
-					break;
-				case 'gitlab_plugin':
-				case 'gitlab_theme':
-					$addition[ 'GitLab' . ucwords( $type ) . 'URI' ] = $repo['uri'];
-					break;
-				case 'gitea_plugin':
-				case 'gitea_theme':
-					$addition[ 'Gitea' . ucwords( $type ) . 'URI' ] = $repo['uri'];
-					break;
-			}
-
-			$this->add_to_github_updater[ $repo['slug'] ] = array_merge( $additions[ $repo['slug'] ], $addition );
-		}
-	}
-
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
+
+// Load Autoloader.
+require_once __DIR__ . '/vendor/autoload.php';
+
+( new Bootstrap( __FILE__ ) )->run();
